c7c7a37ef988aefecadb5ea4916454eb9b41686e
Unique-Usman <usmanakinyemi202@gmail.com>
2023-09-30 15:58:33 +0530

    Added the implementation of Quick sort algorithm

---
+++ 3-quick_sort.c
@@ -0,0 +1,63 @@
+#include "sort.h"
+
+/**
+ * sort - the recursive sort function
+ * @array: the array to be sorted
+ * @low: to indicate the start of the partition
+ * @high: to indicate the end of the partition
+ */
+void sort(int *array, int low, int high, int size)
+{
+	int pivot;
+	int start;
+	int end;
+	int temp;
+
+	pivot = array[high];
+	start = low;
+	end = high;
+	temp = 0;
+
+	if (low >= high)
+		return;
+	while (start < end)
+	{
+		while (array[start] < pivot && start < end)
+		{
+			start++;
+		}
+		while (array[end] >= pivot && start < end)
+		{
+			end--;
+		}
+
+		if (start < end)
+		{
+			temp = array[start];
+			array[start] = array[end];
+			array[end] = temp;
+			print_array(array, size);
+		}
+	}
+
+	if (high != end)
+	{
+		array[high] = array[start];
+		array[start] = pivot;
+		print_array(array, size);
+	}
+
+	sort(array, low, start - 1, size);
+	sort(array, start + 1, high, size);
+}
+/**
+ * quick_sort - the quick sort algorithm
+ * @array: the array to be sorted
+ * @size: the size of the array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+	sort(array, 0, size - 1, size);
+}
